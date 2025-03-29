13897a37d08876762cde33aa10eaafd02d6e38f5
Chengalapattu Vivek Sai <chvivek674@gmail.com>
2025-03-30 00:08:30 +0530

    Commit before deploy

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 const RootLayout = ({children}:{children : ReactNode}) => {
   return (
  <div className="root-layout">
-        <nav>
+        <nav className='flex items-center gap-3'>
             <Link href="/" className="flex items-center gap-2" />
             <Image src="/logo.svg" alt="logo" width={38} height={32} />
         <h2 className="text-primary-100">Interview One</h2>
